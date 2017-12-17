6ac2fc2e1a089b92fa0e1c4710a27271fdc46fc7
Guilherme Oliveira <guilhermeoliveira@Guilhermes-MacBook-Pro.local>
2017-12-17 12:24:24 -0200

    TAbs para poder minimizar

--- notebook.js
+++ notebook.js
@@ -1,73 +1,75 @@
 
-// -- AULA 1.02: Variáveis e tipos de dados --------------------------
-var aula2 = ```
-- NUll é nenhum valor
-- UNDEFINERD é a ausência de valor
-    - Mais usado quando a variável não existe
-- Javascript na verdade não existe arrays, são tudo objeto
-```
+// -- AULA 1.02: Variáveis e tipos de dados ----------------------------
+    var aula2 = ```
+    - NUll é nenhum valor
+    - UNDEFINERD é a ausência de valor
+        - Mais usado quando a variável não existe
+    - Javascript na verdade não existe arrays, são tudo objeto
+    ```
 
 
 // -- AULA 1.03: Operadoes ---------------------------------------------
-var aula3 = ``` +,  -,  *,  /,  ++,  --,  +=,  -=,  *=,  /= ```
+    var aula3 = ``` +,  -,  *,  /,  ++,  --,  +=,  -=,  *=,  /= ```
 
 // -- AULA 1.04: Operadoes de Igualdade e relacionais ------------------
-var aula4 = ```
-== , != 
-1 == '1' -> true
-1 === '1' -> false 
-Javascript é uma linguagem de tipagem fraca e dinâmica
-<, >, <=, >=
-```
+    var aula4 = ```
+    == , != 
+    1 == '1' -> true
+    1 === '1' -> false 
+    Javascript é uma linguagem de tipagem fraca e dinâmica
+    <, >, <=, >=
+    ```
 
-// -- AULA 1.05: Funções ----------------------------------------------
-var aula5 = ```
-Sintaxe e semântica de como criar uma função (argumentos, return, e outra coisas )
-Um pouquinho sobre escopo de variável
-```
+// -- AULA 1.05: Funções -----------------------------------------------
+    var aula5 = ```
+    Sintaxe e semântica de como criar uma função (argumentos, return, e outra coisas )
+    Um pouquinho sobre escopo de variável
+    ```
 
-// -- AULA 1.06: desafio da semans ------------------------------------
+// -- AULA 1.06: desafio da semans -------------------------------------
 
-var aula6 = ```
-DESAFIO
-```
+    var aula6 = ```
+    DESAFIO
+    ```
 
-// # Desafio Semana #1
-// Declarar uma variável chamada `myvar`, sem valor.
-var myvar;
+    // # Desafio Semana #1
+    // Declarar uma variável chamada `myvar`, sem valor.
+    var myvar;
 
-// Após declarada, atribua o valor 10 à variável `myvar`.
-myvar = 10;
+    // Após declarada, atribua o valor 10 à variável `myvar`.
+    myvar = 10;
 
-// Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
-var soma = 15 + 8;
+    // Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
+    var soma = 15 + 8;
 
-// Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
-soma ++;
+    // Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
+    soma ++;
 
-// Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
-soma *= 3;
+    // Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
+    soma *= 3;
 
-// Qual é o valor da variável `soma` até aqui?
-72;
+    // Qual é o valor da variável `soma` até aqui?
+    72;
 
-// Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
-var souninja = true;
+    // Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
+    var souninja = true;
 
-// Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
-var comida = ['arroz', 'feijão', 'ovo'];
+    // Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
+    var comida = ['arroz', 'feijão', 'ovo'];
 
-// Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
-comida[1];
+    // Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
+    comida[1];
 
-// Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
-soma === myvar;
+    // Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
+    soma === myvar;
 
-// Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
-myvar <= soma;
+    // Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
+    myvar <= soma;
 
-// Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
-function divisao(a, b) { return a / b };
+    // Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
+    function divisao(a, b) { return a / b };
 
-// Invoque a função criada acima, passando os parâmetros 10 e 2.
-divisao(10, 2);
+    // Invoque a função criada acima, passando os parâmetros 10 e 2.
+    divisao(10, 2);
+
+// -- AULA 2.01 Operadores lógicos -------------------------------------
