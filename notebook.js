61ea5ba094b909aac2d08e4d6a3f468f4927becd
Guilherme Oliveira <guilhermeoliveira@Guilhermes-MacBook-Pro.local>
2017-12-17 11:49:28 -0200

    Até agora só as coisas simples mesmo

---
+++ notebook.js
@@ -0,0 +1,24 @@
+
+// -- AULA 1.02: Variáveis e tipos de dados --------------------------
+var aula2 = ```
+- NUll é nenhum valor
+- UNDEFINERD é a ausência de valor
+    - Mais usado quando a variável não existe
+- Javascript na verdade não existe arrays, são tudo objeto
+```
+
+
+// -- AULA 1.03: Operadoes ---------------------------------------------
+var aula3 = ```
++
+-
+*
+/
+++
+--
++=
+-=
+*=
+/=
+```
+// -- AULA 1.03: Operadoes ---------------------------------------------
