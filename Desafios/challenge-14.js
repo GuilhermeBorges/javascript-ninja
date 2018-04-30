bdfb835d5cb470967a5add861a90f691fe8f3c47
Guilherme Oliveira <guilherme@evnts.com.br>
2018-04-29 22:42:46 -0300

    Desafio 14

--- Desafios/challenge-14.js
+++ Desafios/challenge-14.js
@@ -11,7 +11,7 @@
   Mostre esse array no console.
   */
   console.log('Number Objects Array:')
-  let numberObjects = Array.from({length: 10}, (i, j) => { return {number: [j+1]} })
+  let numberObjects = Array.from({length: 10}, (i, j) => { return {number: j + 1} })
   console.log(numberObjects)
   /*
   Crie um array chamado `justNumbers`, que terá como elementos somente os
@@ -19,11 +19,13 @@
   */
   console.log('\nJust Numbers:')
   let justNumbers = numberObjects.reduce(function (combinado, atual) {
-    combinado.push(atual.number[0])
+    combinado.push(atual.number)
     return combinado
   }, [])
 
+  let justNumbers2 = numberObjects.map(a => a.number)
   console.log(justNumbers)
+  console.log(justNumbers2)
 
   /*
   Crie um novo array chamado `justMod2Or3`, que receberá do array criado acima
@@ -32,7 +34,13 @@
   */
   console.log('\nJust module of division by 2 or 3:')
   // ?
-
+  let justMod2Or3 = justNumbers.filter((n) => n % 2 === 0 || n % 3 === 0)
+  let justMod2Or3Reduce = justNumbers.reduce((prev, atual) => {
+    if (atual % 2 === 0 || atual % 3 === 0) prev.push(atual)
+    return prev
+  }, [])
+  console.log(justMod2Or3)
+  console.log(justMod2Or3Reduce)
   /*
   Declare uma variável chamada operation que receba, do array criado acima,
   um valor reduzido pela seguinte operação:
@@ -42,15 +50,22 @@
   Mostre o resultado no console.
   */
   console.log('\nOperation:')
-  // ?
 
+  let operation = justMod2Or3.reduce((acumulado, atual) => {
+    return (acumulado + 1) * atual
+  }, 0)
+  console.log(operation)
   /*
   Faça o mesmo cálculo passado acima, mas começando do último item para o
   primeiro. O nome da variável deve ser operation2. Mostre o resultado no
   console.
   */
   console.log('\nOperation 2:')
-  // ?
+
+  let operation2 = justMod2Or3.reduceRight((prev, atual) => {
+    return (prev + 1) * atual
+  }, 0)
+  console.log(operation2)
 
   /*
   Crie um array chamado `name`. Cada elemento desse array deve ser uma sílaba
@@ -61,21 +76,27 @@
   falada, como se você estivesse falando em código xD
   */
   console.log('\nSeu nome na língua do "P":')
-  // ?
+
+  let name = ['Gui', 'lher', 'me']
+  let p = name.reduce((prev, actual) => (prev + 'p' + actual), '')
+  console.log(p)
 
   /*
   Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
   e atribuirá o seu nome invertido (usando o array criado acima).
   */
   console.log('\nInversed Name:')
-  // ?
 
+  let inversedName = name.reduceRight((prev, atual) => {
+    return prev + atual.split('').reverse().join('')
+  }, '')
+  console.log(inversedName)
   /*
   Mostre no console o array `numberObjects`.
   */
   console.log('\nNumber objects')
-  // ?
 
+  console.log(numberObjects)
   /*
   Verifique se existem em algum índice de numberObjects um objeto ìgual a
   { number: 2 }. Se houver, mostre no console:
@@ -86,19 +107,32 @@
   o que acontece )
   */
   console.log('\nExiste um { number: 2 } em numberObjects?')
-  // ?
+  let existe = numberObjects.indexOf({ number: 2 })
+  console.log('Quando criamos um objecto ele cria uma identificação única na memória, um objeto nunca será igual a outro')
 
+  let funcionaria = numberObjects[1]
+  console.log(numberObjects.indexOf(funcionaria))
+
+  let existe2 = numberObjects.reduce((p, a) => (a.number === 2) ? true : p, false)
+  let texto = (existe2) ? 'Existe um objeto { number: 2 } em numberObjects!' : 'Não existe um objeto { number: 2 } em numberObjects!'
+  console.log(texto)
   /*
   Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
-  será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
+  será que obtemos um resul tado diferente? Faça a busca a partir do índice 2.
   */
   console.log('\nE buscando a partir do último índice, o { number: 2 } existe?')
-  // ?
+  // let existeInvertido = numberObjects.lastIndexOf({ number: 2 })
+  let existeInvertido = numberObjects.lastIndexOf({number: 2})
+  console.log(existeInvertido)
+  let existeInvertido2 = numberObjects.some(e => e.number === 2)
+  console.log(existeInvertido2)
+  let textoInvertido = (existeInvertido >= 0) ? 'Existe um objeto { number: 2 } em numberObjects!' : 'Não existe um objeto { number: 2 } em numberObjects!'
+  console.log(textoInvertido)
 
   /*
   Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
   formato de String.
   */
   console.log('\njustMod2Or3 é um array? Se for, a representação dele em String é:')
-  // ?
+  console.log(Array.isArray(justMod2Or3) ? justMod2Or3.join('') : 'Não é um array')
 })()
